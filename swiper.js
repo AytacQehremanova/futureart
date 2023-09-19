b05c1907a046bf806a3bda2eadc0a210b9b062c0
aytac <qehremanova.aytac2003@gmail.com>
2023-09-19 12:57:59 +0400

    services ve projects sectionlari elave olundu

--- swiper.js
+++ swiper.js
@@ -10,3 +10,27 @@ const bannerSwiper = new Swiper(".banner-swiper", {
       clickable: true,
     }
   });
+
+
+  const projectssSwiper = new Swiper(".projects-swiper", {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    slidesPerView: 1,
+    breakpoints:{
+      700:{
+        slidesPerView:2,
+        spaceBetween:30,
+      },
+
+      1024:{
+        slidesPerView:3,
+        spaceBetween:30,
+      }
+    }
+  });
